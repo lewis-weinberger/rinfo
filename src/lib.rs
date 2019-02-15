ca3b9ebdf600ec5976ec2ca094a62f381a081430
Lewis Weinberger <lhw28@cam.ac.uk>
2019-02-15 16:40:37 +0000

    laying the foundations

---
+++ src/lib.rs
@@ -0,0 +1,144 @@
+use std::error::Error;
+use std::fs;
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let info = Info::new(config.filename)?;
+
+    println!("{:?}", info);
+
+    Ok(())
+}
+
+pub struct Config {
+    pub snapnum: u32,
+    pub filename: String,
+    pub query: String,
+}
+
+impl Config {
+    pub fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("Incorrect arguments: [SNAPNUM] [QUERY]");
+        }
+
+        let snapnum: u32 = args[1].parse().unwrap();
+        let mut filename = String::from("output_");
+        let suffix = format!("{:05}/info_{:05}.txt", snapnum, snapnum);
+        filename.push_str(&suffix);
+        let query   = args[2].clone();
+
+        Ok(Config { snapnum, filename, query })
+    }
+}
+
+#[derive(Debug)]
+struct Info {
+    ncpu: u32,
+    ndim: u32,
+    levelmin: u32,
+    levelmax: u32,
+    ngridmax: u32,
+    nstep_coarse: u32,
+    boxlen: f64,
+    time: f64,
+    H0: f64,
+    omega_m: f64,
+    omega_l: f64,
+    omega_k: f64,
+    omega_b: f64,
+    unit_l: f64,
+    unit_d: f64,
+    unit_t: f64,
+    redshift: f64,
+    smallh: f64,
+    boxsize: f64,
+    Hz: f64,
+}
+
+impl Info {
+    fn new(filename: String) -> Result<Info, Box<dyn Error>> {
+        let contents   = fs::read_to_string(filename)?;
+        let mut buffer = Vec::new();
+
+        for line in contents.lines() {
+            buffer.push(line);
+        }
+
+        let ncpu: u32         = Info::extract(&buffer[0]).parse().unwrap();
+        let ndim: u32         = Info::extract(&buffer[1]).parse().unwrap();
+        let levelmin: u32     = Info::extract(&buffer[2]).parse().unwrap();
+        let levelmax: u32     = Info::extract(&buffer[3]).parse().unwrap();
+        let ngridmax: u32     = Info::extract(&buffer[4]).parse().unwrap();
+        let nstep_coarse: u32 = Info::extract(&buffer[5]).parse().unwrap();
+        
+        let boxlen: f64   = Info::extract(&buffer[7]).parse().unwrap();
+        let time: f64     = Info::extract(&buffer[8]).parse().unwrap();
+        let aexp: f64     = Info::extract(&buffer[9]).parse().unwrap();
+        let H0: f64       = Info::extract(&buffer[10]).parse().unwrap();
+        let omega_m: f64  = Info::extract(&buffer[11]).parse().unwrap();
+        let omega_l: f64  = Info::extract(&buffer[12]).parse().unwrap();
+        let omega_k: f64  = Info::extract(&buffer[13]).parse().unwrap();
+        let omega_b: f64  = Info::extract(&buffer[14]).parse().unwrap();
+        let unit_l: f64   = Info::extract(&buffer[15]).parse().unwrap();
+        let unit_d: f64   = Info::extract(&buffer[16]).parse().unwrap();
+        let unit_t: f64   = Info::extract(&buffer[17]).parse().unwrap();
+        
+        let redshift = Info::find_redshift(aexp);
+        let smallh   = H0/100.0;
+        let boxsize  = Info::find_boxsize(unit_l, boxlen);
+        let Hz       = Info::find_Hz(aexp, H0, omega_m, omega_k, omega_l);
+
+        Ok(Info { ncpu,
+                  ndim,
+                  levelmin,
+                  levelmax,
+                  ngridmax,
+                  nstep_coarse,
+                  boxlen,
+                  time,
+                  H0,
+                  omega_m,
+                  omega_l,
+                  omega_k,
+                  omega_b,
+                  unit_l,
+                  unit_d,
+                  unit_t,
+                  redshift,
+                  smallh,
+                  boxsize,
+                  Hz, }
+          )
+    }
+
+    fn extract(buf: &str) -> &str {
+        &buf[14..]
+    }
+
+    fn find_redshift(aexp: f64) -> f64 {
+        1.0/aexp - 1.0
+    }
+
+    fn find_boxsize(unit_l: f64, boxlen: f64) -> f64 {
+        boxlen * unit_l * 3.08 * 10.0f64.powf(24.0)
+    }
+
+    fn find_Hz(aexp: f64, H0: f64, omega_m: f64, omega_k: f64, omega_l: f64) -> f64 {
+        let Ez = omega_m / aexp.powf(3.0)
+            + omega_k / aexp.powf(2.0)
+            + omega_l;
+        H0 * Ez.sqrt()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn string_extract() {
+        let test_string = String::from("boxlen      =  0.100000000000000E+01");
+        assert_eq!(Info::extract(&test_string), " 0.100000000000000E+01");
+    }
+            
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use std::env;
+use std::process;
+
+use rinfo;
+use rinfo::Config;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        eprintln!("{}", err);
+        process::exit(1);
+    });
+
+    if let Err(e) = rinfo::run(config) {
+        eprintln!("Error: {}", e);
+        process::exit(1);
+    }
 }
